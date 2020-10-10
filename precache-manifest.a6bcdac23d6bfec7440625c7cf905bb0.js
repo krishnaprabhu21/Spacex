ebecab5b0b11427eabae843a23596b749784031a
Krishna PrabhuGaonkar <krishna.prabhugaonkar@indegene.com>
2020-10-10 10:07:08 +0530

    Updates

---
+++ precache-manifest.a6bcdac23d6bfec7440625c7cf905bb0.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439e68c6d34327d53e51e6a78139ed4d",
+    "url": "/Spacex/index.html"
+  },
+  {
+    "revision": "9d39e15ef00be9b4e55d",
+    "url": "/Spacex/static/css/main.fc8760b7.chunk.css"
+  },
+  {
+    "revision": "d7cd56228261d5dd8d4a",
+    "url": "/Spacex/static/js/2.881d4d37.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/Spacex/static/js/2.881d4d37.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d39e15ef00be9b4e55d",
+    "url": "/Spacex/static/js/main.61c0462f.chunk.js"
+  },
+  {
+    "revision": "332e0211850a341654ff",
+    "url": "/Spacex/static/js/runtime-main.730f38b1.js"
+  },
+  {
+    "revision": "3a3d90f214acb237ae7912e645d0b049",
+    "url": "/Spacex/static/media/alien.3a3d90f2.svg"
+  }
+]);
